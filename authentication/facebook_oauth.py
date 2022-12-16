e6d4adf03de12690da1eac474f11f782729c25be
Khôi Tạ <dbsiksfikf@gmail.com>
2022-12-16 08:34:05 +0000

    fix/oauth

--- authentication/facebook_oauth.py
+++ authentication/facebook_oauth.py
@@ -15,33 +15,33 @@ from . import router
 
 authorization_base_url = 'https://www.facebook.com/dialog/oauth'
 token_url = 'https://graph.facebook.com/oauth/access_token'
-facebook = OAuth2Session(FACEBOOK_CLIENT_ID, redirect_uri=FACEBOOK_REDIRECT_URL)
+facebook = OAuth2Session(FACEBOOK_CLIENT_ID,
+                         redirect_uri=FACEBOOK_REDIRECT_URL)
 facebook = facebook_compliance_fix(facebook)
 
 
 @dataclass
 class FaceBookOauth2:
-    # OAuth endpoints given in the Facebook API documentation
+  # OAuth endpoints given in the Facebook API documentation
 
-    def get_oauth_url(self):
-        authorization_url, state = facebook.authorization_url(authorization_base_url)
-        return authorization_url
+  def get_oauth_url(self):
+    authorization_url, state = facebook.authorization_url(
+      authorization_base_url)
+    return authorization_url
 
-    def get_user_info(self, redirect_response: str):    
-        facebook.fetch_token(
-                token_url, 
-                client_secret=FACEBOOK_CLIENT_SECRET,
-                authorization_response=redirect_response
-            )
+  def get_user_info(self, redirect_response: str):
+    facebook.fetch_token(token_url,
+                         client_secret=FACEBOOK_CLIENT_SECRET,
+                         authorization_response=redirect_response)
 
-        r = facebook.get('https://graph.facebook.com/me?')
-        return r.json()
+    r = facebook.get('https://graph.facebook.com/me?')
+    return r.json()
 
 
 @router.get("/facebook-oauth")
-async def discord_oauth(request: Request):
-    authorization_response = str(request.url)
-    if "https" not in authorization_response:
-        authorization_response = authorization_response.replace('http', 'https')
-    user = FaceBookOauth2().get_user_info(authorization_response)
-    return JSONResponse(user, status_code=200)
+async def facebook_oauth(request: Request):
+  authorization_response = str(request.url)
+  if not authorization_response.startswith('https'):
+    authorization_response = authorization_response.replace('http', 'https')
+  user = FaceBookOauth2().get_user_info(authorization_response)
+  return JSONResponse(user, status_code=200)
--- authentication/google_oauth.py
+++ authentication/google_oauth.py
@@ -12,41 +12,36 @@ from requests_oauthlib import OAuth2Session
 from all_env import GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_REDIRECT_URL
 from . import router
 
-scope = (
-    'https://www.googleapis.com/auth/userinfo.email',
-    'https://www.googleapis.com/auth/userinfo.profile',
-    'openid'
-    )
-oauth = OAuth2Session(
-        client_id=GOOGLE_CLIENT_ID,
-        redirect_uri=GOOGLE_REDIRECT_URL,
-        scope=scope
-    )
+scope = ('https://www.googleapis.com/auth/userinfo.email',
+         'https://www.googleapis.com/auth/userinfo.profile', 'openid')
+oauth = OAuth2Session(client_id=GOOGLE_CLIENT_ID,
+                      redirect_uri=GOOGLE_REDIRECT_URL,
+                      scope=scope)
+
 
 @dataclass
 class GoogleOauth2:
 
-    def get_oauth_url(self):
-        authorization_url, state = oauth.authorization_url(
-            'https://accounts.google.com/o/oauth2/auth',
-            access_type="offline", prompt="select_account"
-        )
-        return authorization_url
+  def get_oauth_url(self):
+    authorization_url, state = oauth.authorization_url(
+      'https://accounts.google.com/o/oauth2/auth',
+      access_type="offline",
+      prompt="select_account")
+    return authorization_url
 
-    def get_user_info(self, authorization_response):
-        oauth.fetch_token(
-            'https://accounts.google.com/o/oauth2/token',
-            authorization_response=authorization_response,
-            client_secret=GOOGLE_CLIENT_SECRET
-        )
-        r = oauth.get('https://www.googleapis.com/oauth2/v1/userinfo')
-        return r.json()
+  def get_user_info(self, authorization_response):
+    oauth.fetch_token('https://accounts.google.com/o/oauth2/token',
+                      authorization_response=authorization_response,
+                      client_secret=GOOGLE_CLIENT_SECRET)
+    r = oauth.get('https://www.googleapis.com/oauth2/v1/userinfo')
+    return r.json()
 
 
 @router.get("/google-oauth")
-async def discord_oauth(request: Request):
-    authorization_response = str(request.url)
-    if "https" not in authorization_response:
-        authorization_response = authorization_response.replace('http', 'https')
-        user = GoogleOauth2().get_user_info(authorization_response)
-    return JSONResponse(user, status_code=200)
+async def google_oauth(request: Request):
+  authorization_response = str(request.url)
+  if not authorization_response.startswith('https'):
+    authorization_response = authorization_response.replace('http', 'https')
+  print(authorization_response)
+  user = GoogleOauth2().get_user_info(authorization_response)
+  return JSONResponse(user, status_code=200)
