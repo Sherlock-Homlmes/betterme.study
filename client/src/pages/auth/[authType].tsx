2e1d55a374bc30b0601bafe356c697bf56e115ed
Sherlock-Homlmes <owner@betterme.study>
2023-03-05 22:39:32 +0700

    feat: login by oauth

--- client/src/pages/auth/[authType].tsx
+++ client/src/pages/auth/[authType].tsx
@@ -2,26 +2,28 @@ import { useRouter } from 'next/router';
 import fetchLink from '@/config';
 
 const Auth = () => {
-    // get auth url from router
-    const router = useRouter();
-    const { query } = useRouter();
-    let apiAuthLink = fetchLink
+  // get auth url from router
+  const router = useRouter();
+  const { query } = useRouter();
+  let apiAuthLink = fetchLink;
 
-    // 
-    if (["discord-oauth", "google-oauth", "facebook-oauth"].includes(query.authType)){
-        apiAuthLink += `/auth/${query.authType}?`
-    }
+  //
+  if (
+    ['discord-oauth', 'google-oauth', 'facebook-oauth'].includes(query.authType)
+  ) {
+    apiAuthLink += `/auth/${query.authType}?`;
+  }
 
-    // get JWT token
-    fetch(apiAuthLink+ new URLSearchParams(query))
-        .then((res) => res.json())
-        .then((data) => {
-            if (typeof window !== 'undefined' && data.token) {
-                window.localStorage.setItem('Authorization', `Bearer ${data.token}`)
-                router.replace(`/dashboard`)
-            }
-        })
+  // get JWT token
+  fetch(apiAuthLink + new URLSearchParams(query))
+    .then((res) => res.json())
+    .then((data) => {
+      if (typeof window !== 'undefined' && data.token) {
+        window.localStorage.setItem('Authorization', `Bearer ${data.token}`);
+        router.replace(`/dashboard`);
+      }
+    });
 
-    return <p>abc</p>;
-}
+  return <p>abc</p>;
+};
 export default Auth;
